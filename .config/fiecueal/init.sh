1ccc5b7d1b1e8ad4d5fe76322ef8be1d29cd1423
fiecueal <143047492+fiecueal@users.noreply.github.com>
2025-10-06 17:45:15 -0700

    overhaul init.sh

--- .config/fiecueal/init.sh
+++ .config/fiecueal/init.sh
@@ -1,71 +1,116 @@
 #!/usr/bin/bash
 
-cd $HOME # start script in home dir
-sudo apt purge -y \
-gucharmap \
-nodejs \
-npm \
-yt-dlp
+is_server=false
+is_laptop=false
 
-sudo apt-mark nodejs npm yt-dlp *wacom*
+for arg in "$@"; do
+  case "$arg" in
+  server) is_server=true ;;
+  laptop) is_laptop=true ;;
+  esac
+done
+
+cd $HOME
+mkdir -p Archives Pictures/screenshots Projects .local/bin
+
+sudo mkdir -p /etc/systemd/logind.conf.d
+sudo cp $HOME/.config/fiecueal/00-powerkey-lidswitch.conf /etc/systemd/logind.conf.d/
+
+sudo apt purge -y yt-dlp
+sudo apt-mark hold yt-dlp
+curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o .local/bin/yt-dlp
+chmod a+rx .local/bin/yt-dlp
 
 sudo apt update
 sudo apt upgrade -y
 sudo apt dist-upgrade -y
-sudo apt install -y curl wget unzip
+sudo apt install -y \
+autojump \
+btop \
+build-essential \
+curl \
+ffmpeg \
+imagemagick \
+libgmp-dev \
+libssl-dev \
+libyaml-dev \
+micro \
+network-manager \
+nnn \
+rustc \
+tar \
+trash-cli \
+ufw \
+unzip \
+wget \
+zip \
+zlib1g-dev \
 
-mkdir -p Archives Pictures/screenshots Projects .local/bin
+sudo ufw enable
 
-# Brave browser
+curl https://mise.run | sh
+eval "$(mise activate)"
+mise use -g go@latest
+mise use -g ruby@latest
+
+gem install rails
+
+if $is_server; then
+
+sudo ufw allow from 10.0.0.0/24 to any port 22 proto tcp
+
+sudo apt install -y \
+samba \
+
+sudo ufw allow from 10.0.0.0/24 to any app Samba
+sudo mkdir -p /etc/samba
+[ -f /etc/samba/smb.conf ] && sudo mv --backup=numbered /etc/samba/smb.conf /etc/samba/smb.conf.old
+sudo cp $HOME/.config/fiecueal/smb.conf /etc/samba/smb.conf
+sudo systemctl restart smbd nmbd
+
+else # client
+
+sudo mkdir -p /etc/X11/xorg.conf.d
+sudo cp $HOME/.config/fiecueal/20-screentear.conf \
+        $HOME/.config/fiecueal/20-mouseaccel.conf \
+        /etc/X11/xorg.conf.d/
+
+sudo mkdir -p /etc/udev/rules.d/ /etc/modules-load.d/
+sudo cp $HOME/.config/fiecueal/71-8bitdo-controllers.rules /etc/udev/rules.d/
+sudo cp $HOME/.config/fiecueal/uinput.conf /etc/modules-load.d/uinput.conf
+
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 
-# Victor Mono
-#wget https://rubjo.github.io/victor-mono/VictorMonoAll.zip -P $HOME/Downloads/
-#unzip $HOME/Downloads/VictorMonoAll.zip -d $HOME/Downloads/VictorMonoAll/
-#cp -rT $HOME/Downloads/VictorMonoAll/OTF/ $HOME/.local/share/fonts/
-
-# Sublime text
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-wget https://packagecontrol.io/Package%20Control.sublime-package -P $HOME/.config/sublime-text/Installed\ Packages
+wget https://github.com/wbond/package_control/releases/latest/download/Package.Control.sublime-package -P $HOME/.config/sublime-text/Installed\ Packages
+cd $HOME/.config/sublime-text/Installed\ Packages
+mv Package.Control.sublime-package Package\ Control.sublime-package
 
-# yt-dlp
-curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o .local/bin/yt-dlp
-chmod a+rx .local/bin/yt-dlp
-
-# deno
-curl -fsSL https://deno.land/install.sh | sh
-ln -s $DENO_ISNTALL/bin/deno $HOME/.local/bin/deno
-
-# pnpm, node
-curl -fsSL https://get.pnpm.io/install.sh | sh -
-pnpm env use --global lts
-
-# dragonruby
 ln -s $DRAGONRUBY_HOME/dragonruby $HOME/.local/bin/dragonruby
 ln -s $DRAGONRUBY_HOME/dragonruby-httpd $HOME/.local/bin/dragonruby-httpd
 ln -s $DRAGONRUBY_HOME/dragonruby-publish $HOME/.local/bin/dragonruby-publish
 
-# godot
 wget https://github.com/godotengine/godot/releases/download/4.5-stable/Godot_v4.5-stable_linux.x86_64.zip
 unzip Godot_v4.5-stable_linux.x86_64.zip
 mv Godot_v4.5-stable_linux.x86_64 $HOME/.local/bin/godot
 rm Godot_v4.5-stable_linux.x86_64.zip
 
+curl -fsSL https://deno.land/install.sh | sh
+ln -s $DENO_ISNTALL/bin/deno $HOME/.local/bin/deno
+
+curl -fsSL https://get.pnpm.io/install.sh | sh -
+pnpm env use --global lts
+
 # git clone https://github.com/fiecueal/qmk_firmware
 
-# install packages
 sudo apt install -y \
-autojump \
 brave-browser \
-btop \
-build-essential \
 cowsay \
 dunst \
 fcitx5 \
 fcitx5-mozc \
-ffmpeg \
 firefox-esr \
 flameshot \
 flatpak \
@@ -78,34 +123,26 @@ fonts-monofur \
 fonts-noto \
 fortunes \
 git-cola \
-gnome-commander \
 gpick \
+gpicview \
 i3-wm \
 i3lock \
-imagemagick \
 inkscape \
-libgmp-dev \
-libssl-dev \
-libyaml-dev \
-micro \
-mise \
+libglib2.0-bin \
 mpv \
-mypaint \
-network-manager \
+mtpaint \
 network-manager-applet \
 obs-studio \
 openjdk-21-jdk \
 pavucontrol \
 playerctl \
 pulseaudio \
-rustc \
 steam-installer \
 sublime-merge \
 sublime-text \
 suckless-tools \
-trash-cli \
+thunar \
 udisks2 \
-ufw \
 woff2 \
 xdg-utils \
 xfce4-clipman \
@@ -113,51 +150,26 @@ xfce4-terminal \
 xinit \
 xsct \
 xss-lock \
-zlib1g-dev \
 
-sudo ufw enable
-sudo ufw allow from 10.0.0.0/24 to any port 22 proto tcp
-
-# install flatpaks
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install -y flathub \
 com.atlauncher.ATLauncher \
 com.github.tchx84.Flatseal \
 com.heroicgameslauncher.hgl \
 com.tomjwatson.Emote \
-io.mgba.mGBA \
+com.vysp3r.ProtonPlus \
 net.lutris.Lutris \
-com.vysp3r.ProtonPlus
 
-curl https://mise.run | sh
-eval "$(mise activate)"
-mise use -g go@latest
-mise use -g ruby@latest
-
-# install tlp for power management and charge thresholds if 'laptop' passed as arg
-if [ $1 = "laptop" ]; then
-  sudo nala install -y tlp
-  sudo mkdir -p /etc/tlp.d
-  sudo cp $HOME/.config/fiecueal/00-charge-thresh.conf \
-          $HOME/.config/fiecueal/00-low-power-cpu.conf \
-          /etc/tlp.d/
-  sudo tlp start
-fi
-
-# setup firefox profiles
-firefox &
 cd $HOME/.mozilla/firefox
 mv $(find -name *.default) fiecueal.p1
 cp -r fiecueal.p1 fiecueal.p5
 cp -r fiecueal.p1 fiecueal.p9
 
-install=$(grep "\[.*\]" installs.ini)
-sed -i "1i[Install$(echo $install | cut -c2-)" profiles.ini
-cat > installs.ini <<EOF
-$install
-Default=fiecueal.p1
-Locked=1
-EOF
+mv profiles.ini profiles.ini.old
+cp $HOME/.config/fiecueal/profiles.ini .
+
+sed -i "1i$(grep "\[Install.*\]" profiles.ini.old)" profiles.ini
+sed -i "s/^Default=.*$/Default=fiecueal.p1/" installs.ini
 
 cd $HOME/Projects
 git clone https://github.com/fiecueal/Betterfox
@@ -165,22 +177,17 @@ cd Betterfox
 ./install.sh p1
 ./install.sh p5
 ./install.sh p9
-cd $HOME
 
-# fix screen tearing & disable mouse acceleration
-sudo mkdir -p /etc/X11/xorg.conf.d
-sudo cp $HOME/.config/fiecueal/20-screentear.conf \
-        $HOME/.config/fiecueal/20-mouseaccel.conf \
-        /etc/X11/xorg.conf.d/
+fi # $is_server
 
-# fix 8bitdo controller recognition
-sudo mkdir -p /etc/udev/rules.d/ /etc/modules-load.d/
-sudo cp $HOME/.config/fiecueal/71-8bitdo-controllers.rules /etc/udev/rules.d/
-sudo cp $HOME/.config/fiecueal/uinput.conf /etc/modules-load.d/uinput.conf
-
-# shutdown on power button held instead of pressed
-sudo mkdir -p /etc/systemd/logind.conf.d
-sudo cp $HOME/.config/fiecueal/00-powerkey-lidswitch.conf /etc/systemd/logind.conf.d/
+if $is_laptop; then
+sudo apt install -y tlp
+sudo mkdir -p /etc/tlp.d
+sudo cp $HOME/.config/fiecueal/00-charge-thresh.conf \
+        $HOME/.config/fiecueal/00-low-power-cpu.conf \
+        /etc/tlp.d/
+sudo tlp start
+fi
 
 cat <<EOF
 Not yet done:
@@ -189,4 +196,6 @@ Not yet done:
 * Download Intellij Idea
 * Monospacify monofur font
 * Install firefox extensions
+* Add samba users
+* Reboot
 EOF
